213b5c359030382bf53c8d7f5b98250f3852b9f2
Shedriq <jerrydwain4@gmail.com>
2022-09-20 23:18:29 +0300

    Printing the alphaber

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if (i == ord('e') or i == ord('q')):
-        i += 1
-    else:
-        print(chr(i), end="")
-        i += 1
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
