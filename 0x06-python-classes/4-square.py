f3a047edd2b40d3a6d4380625b17d130c784cec8
Shedriq <jerrydwain4@gmail.com>
2022-10-12 22:40:45 +0300

    Write a class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 4-square.py by Shedrick
-"""A module that defines a square"""
+"""A module that defines a square """
 
 
 class Square:
@@ -9,11 +9,11 @@ class Square:
     def __init__(self, size=0):
         """Initializing this square class
         Args:
-            size: represents the size of the square defined
+            size: represnets the size of the square defined
         Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero
-            """
+        """
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
@@ -22,24 +22,24 @@ class Square:
 
         self.__size = size
 
-        @property
-        def size(self):
-            """Retrieves size of square"""
+    @property
+    def size(self):
+        """Retrieves size of square"""
 
-            return self.__size
+        return self.__size
 
-        @size.setter
-        def size(self, valie):
-            if not isinstance(value, int):
-                raise TypeError('size must be an integer')
-            if value < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-        def areaa(self):
-            """
-            Calculate area of the square
-            Returns: The square of the size
-            """
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
 
-            return (self.__size ** 2)
+        return (self.__size ** 2)
