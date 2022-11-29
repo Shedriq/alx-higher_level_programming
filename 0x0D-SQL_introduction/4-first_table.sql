9aee8be1d58dfea3843288bc67f54494db66f087
Shedriq <jerrydwain4@gmail.com>
2022-11-29 23:11:57 +0300

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- creates a table in current db
+-- creates a tabel first_table in current db
 -- does not fail if table exists
-CREATE TABLE IF NOT EXITSTS first_table (id INT,
+CREATE TABLE IF NOT EXISTS first_table (id INT,
 name VARCHAR(256));
