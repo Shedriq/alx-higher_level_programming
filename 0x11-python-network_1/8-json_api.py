42a7b6b52f7c49215c2e9ba00db6e45da5b2165f
Shedriq <jerrydwain4@gmail.com>
2023-01-18 11:56:32 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-Usage: ./8-json_api.pyy <letter>
-    - The letter is sent as the value of the variable `q`.
-    - If no letter is provided, sends `q=""`.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
 import sys
 import requests
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.posts("http:0.0.0.0:5000/serach_user", data=payload)
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        reponse = r.json()
+        response = r.json()
         if response == {}:
             print("No result")
         else:
